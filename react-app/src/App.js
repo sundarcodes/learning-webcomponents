636d2b70f01febe15c32418ff4c2318d0ac0fa73
sundarcodes <sundar82@gmail.com>
2019-02-19 19:29:51 +0530

    Fixed few styling

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -6,14 +6,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <my-component first="Stencil" last="Hello All from React"></my-component>
-        <h1>Chip</h1>
-
+        <h1>Hi from React</h1>
+        <h3>A simple component</h3>
+        <my-component first="Stencil" last="from React"></my-component>
+        <h3>A Chip component</h3>
         <stc-chip image="http://nodeframework.com/assets/img/js.png" text="image chip"></stc-chip>
         <stc-chip image="" text="just a chip"></stc-chip>
         <stc-chip image="" text="closeable chip" closeable></stc-chip>
         <stc-chip image="http://nodeframework.com/assets/img/js.png" text="closeable chip" closeable></stc-chip>
-
+        <h3>A Signature capture component</h3>
         <st-signature width="300" height="300" label="My Signature"></st-signature>
       </div>
     );
